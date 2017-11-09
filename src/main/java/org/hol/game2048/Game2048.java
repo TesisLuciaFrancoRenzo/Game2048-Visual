78ec1f9e895811834a998f995e25716dfc1c8e7e
Franco Gastón Pellegrini <francogpellegrini@gmail.com>
2017-11-09 12:45:10 -0300

    Nuevo Best NTuple perceptron entrenado para ganar 95.6% de las veces,

--- src/main/java/org/hol/game2048/Game2048.java
+++ src/main/java/org/hol/game2048/Game2048.java
@@ -48,37 +48,96 @@ class Game2048
         launch(args);
     }
 
+    private
+    void configureNTupleSystem(
+            final InputStream perceptronFile,
+            final Function< Double, Double > activationFunction,
+            final Function< Double, Double > derivedActivationFunction,
+            final boolean concurrency,
+            final int maxTile,
+            final int nTuplesQuantity,
+            final int nTuplesLength
+    ) {
+        final int[] nTuplesLengths = new int[nTuplesQuantity];
+        for ( int i = 0; i < nTuplesQuantity; i++ ) {
+            nTuplesLengths[i] = nTuplesLength;
+        }
+
+        final ArrayList< SamplePointValue > allSamplePointPossibleValues = new ArrayList<>();
+        allSamplePointPossibleValues.add(new SimpleTile());
+        for ( int i = 1; i <= maxTile; i++ ) {
+            allSamplePointPossibleValues.add(new SimpleTile((int) Math.pow(2, i)));
+        }
+        nTupleSystem = new NTupleSystem(allSamplePointPossibleValues, nTuplesLengths, activationFunction, derivedActivationFunction, concurrency);
+        try {
+            nTupleSystem.load(perceptronFile);
+            perceptronFile.close();
+        } catch ( IOException | ClassNotFoundException ex ) {
+            Logger.getLogger(Game2048.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     @Override
     public
     void init() {
         // TO-DO: Step 10. Load font when css is enabled
         if ( STEP >= 10 ) {
             Font.loadFont(Game2048.class.getResource("ClearSans-Bold.ttf").toExternalForm(), 10.0);
-            final InputStream                perceptronFile            = getClass().getResourceAsStream("/org/hol/game2048/trainedntuplas/Best.ser");
+
+            //**** NTuple *****
+            //            experimentName: ConfigNTupleBasicLinear_32768
+            //            perceptronName: ConfigNTupleBasicLinear_32768
+            //            tileToWinForTraining: 32768
+            //            tileToWinForStatistics: 2048
+            //            hasBias: false
+            //            gamesToPlay: 1000000
+            //            winRateLimit: -1.0
+            //            gamesToPlayPerThreadForStatistics: 10000
+            //            statisticsOnly: true
+            //            simulationsForStatistics: 8
+            //            saveEvery: 2000
+            //            saveBackupEvery: 25000
+            //            alpha: [0.005, 0.005]
+            //            gamma: 1.0
+            //            lambda: 0.0
+            //            eligibilityTraceLength: -1
+            //            replaceEligibilityTraces: false
+            //            annealingT: 400000
+            //            learningRateAdaptation: ANNEALING
+            //            initializePerceptronRandomized: false
+            //            computeBestPossibleActionConcurrently: false
+            //            concurrencyInLayer: [false, false]
+            //            whenStartToExplore: 0.5
+            //            explorationRate: LINEAR
+            //            explorationRateFinalValue: 5.0E-4
+            //            explorationRateInitialValue: 0.005
+            //            explorationRateStartDecrementing: 0
+            //            explorationRateFinishDecrementing: 400000
+            //
+            //            Operating system Name:
+            //            Windows 10
+            //            Operating system type: amd64
+            //            Operating system version: 10.0
+            //            CPU: AMD FX(tm)-8350 Eight-Core Processor
+            //            Available processors (cores): 8
+            //            Maximum memory (bytes): 3810525184
+            //            Total memory available to JVM (bytes): 257425408
+            final InputStream                perceptronFile            = getClass().getResourceAsStream("/org/hol/game2048/trainedntuples/Best.ser");
             final Function< Double, Double > activationFunction        = FunctionUtils.LINEAR;
             final Function< Double, Double > derivedActivationFunction = FunctionUtils.LINEAR_DERIVED;
             final boolean                    concurrency               = false;
             final int                        maxTile                   = 15;
-
-            final int[] nTuplesLength = new int[17];
-            for ( int i = 0; i < 17; i++ ) {
-                nTuplesLength[i] = 4;
-            }
-
-            final ArrayList< SamplePointValue > allSamplePointPossibleValues = new ArrayList<>();
-            allSamplePointPossibleValues.add(new SimpleTile());
-            for ( int i = 1; i <= maxTile; i++ ) {
-                allSamplePointPossibleValues.add(new SimpleTile((int) Math.pow(2, i)));
-            }
-            nTupleSystem = new NTupleSystem(allSamplePointPossibleValues, nTuplesLength, activationFunction, derivedActivationFunction, concurrency);
-            try {
-                nTupleSystem.load(perceptronFile);
-                perceptronFile.close();
-            } catch ( IOException | ClassNotFoundException ex ) {
-                Logger.getLogger(Game2048.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
+            final int                        nTuplesQuantity           = 17;
+            final int                        nTuplesLength             = 4;
             computeBestPossibleActionConcurrently = false;
+
+            configureNTupleSystem(perceptronFile,
+                    activationFunction,
+                    derivedActivationFunction,
+                    concurrency,
+                    maxTile,
+                    nTuplesQuantity,
+                    nTuplesLength);
         }
     }
 
@@ -107,8 +166,11 @@ class Game2048
                     gameManager.move(dir);
                 } else if ( ( keyCode == KeyCode.SPACE ) && gameManager.getNTupleBoard().isCanMove() ) {
                     final List< IAction > possibleActions = gameManager.listAllPossibleActions(gameManager.getNTupleBoard());
-                    final Direction bestAction = (Direction) TDLambdaLearning.computeBestPossibleAction(gameManager, ELearningStyle.AFTER_STATE,
-                            gameManager.getNTupleBoard(), possibleActions, computeBestPossibleActionConcurrently,
+                    final Direction bestAction = (Direction) TDLambdaLearning.computeBestPossibleAction(gameManager,
+                            ELearningStyle.AFTER_STATE,
+                            gameManager.getNTupleBoard(),
+                            possibleActions,
+                            computeBestPossibleActionConcurrently,
                             random,
                             null).getAction();
                     gameManager.move(bestAction);
